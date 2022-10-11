e2418aab88365f70091b2b71089b7998f97f3c1c
nivaaz <nksehmbhi@gmail.com>
2022-10-12 01:40:55 +1100

    fix: fix buiild issue/get started page

--- pages/get-started.jsx
+++ pages/get-started.jsx
@@ -1,5 +1,6 @@
+import clsx from "clsx";
 import Image from "next/image";
-import { useState, useRef } from "react";
+import { useState, useRef, forwardRef } from "react";
 import Button from "../components/design-system/button";
 
 const GetStarted = () => {
@@ -26,7 +27,6 @@ const GetStarted = () => {
   return (
     <div className="w-full flex flex-col space-y-8 p-4 pb-16 bg-purple-100 text-black">
       <section className=" border border-black w-full mx-auto min-h-screen flex">
-      
         <div className="m-auto text-center">
           <h1 className="text-2xl py-8 font-heading">
             Launch Loyalty Passes with Insider
@@ -36,17 +36,19 @@ const GetStarted = () => {
           <p>2. Tell us about your loyalty pass collection</p>
           <p>3. Design your loyalty pass collection</p>
           <p>4. Preview & share your collection</p>
-          <div className="flex justify-center pt-8"> 
-          <Button copy="Let's go" variant="primary"
-            onClick={scrollToEmailInput}
+          <div className="flex justify-center pt-8">
+            <Button
+              copy="Let's go"
+              variant="primary"
+              onClick={scrollToEmailInput}
             />
-            </div>
+          </div>
         </div>
       </section>
       <div className="grid md:grid-cols-2 relative">
-        <div className="m-auto space-y-8">
-          <Section ref={myRef}>
-            <p className="text-5xl text-black">1</p>
+        <div className="m-auto space-y-8" ref={myRef}>
+          <Section>
+            <p  className="text-5xl text-black">1</p>
             <h2 className="text-xl py-8">
               {" "}
               Tell us a little bit about your brand
@@ -57,7 +59,7 @@ const GetStarted = () => {
                 placeholder="cool cookies"
                 name="brandName"
                 onChange={handleInputChange}
-                className= "border-b border-primary-default m-1 bg-transparent hover:border-black"
+                className="border-b border-primary-default m-1 bg-transparent hover:border-black"
                 type="text"
               />
             </label>
@@ -77,7 +79,7 @@ const GetStarted = () => {
                 name="website"
                 placeholder="coolcookies.xyz"
                 onChange={handleInputChange}
-                className= "border-b border-primary-default m-1 bg-transparent hover:border-black"
+                className="border-b border-primary-default m-1 bg-transparent hover:border-black"
                 type="url"
               />
             </label>
@@ -114,7 +116,7 @@ const GetStarted = () => {
                 name="numberOfNfts"
                 placeholder="200"
                 onChange={handleInputChange}
-                className= "border-b border-primary-default m-1 bg-transparent hover:border-black"
+                className="border-b border-primary-default m-1 bg-transparent hover:border-black"
                 type="url"
               />
               loyalty passes
@@ -221,40 +223,48 @@ const GetStarted = () => {
           </Section>{" "}
         </div>
         <div className="h-full">
-            <div className="md:sticky top-0 h-screen flex">
-              <div className="m-auto"> 
+          <div className="md:sticky top-0 h-screen flex">
+            <div className="m-auto">
               <p className="text-5xl text-black py-4">Preview</p>
-              <div className="m-auto border border-black rounded-xl p-4 space-y-4"> 
-              <h3 className="text-black italic text-xl"> {formValues?.collectionName ?? 'Collection Name'}</h3>
-              <p> by {formValues.brandName ?? 'Brand'} </p>
+              <div className="m-auto border border-black rounded-xl p-4 space-y-4">
+                <h3 className="text-black italic text-xl">
+                  {" "}
+                  {formValues?.collectionName ?? "Collection Name"}
+                </h3>
+                <p> by {formValues.brandName ?? "Brand"} </p>
 
-              <p className="font-bold">
-                Our motivation for this collection is:
-              </p>
-              <p> {formValues.motivation ?? ' Share the cookie love with the world'} </p>
-              <p>
-                This collection has {formValues.numberOfNfts} passes. Each pass
-                will have one of the following unique traits:
-                {formValues.trait1Category?? ' cookie flavour'}
-              </p>
-              <Image
-                width={200}
-                height={200}
-                alt="pass image"
-                src={formValues.passImage}
-              />
+                <p className="font-bold">
+                  Our motivation for this collection is:
+                </p>
+                <p>
+                  {" "}
+                  {formValues.motivation ??
+                    " Share the cookie love with the world"}{" "}
+                </p>
+                <p>
+                  This collection has {formValues.numberOfNfts} passes. Each
+                  pass will have one of the following unique traits:
+                  {formValues.trait1Category ?? " cookie flavour"}
+                </p>
+                {formValues?.passImage && <Image
+                  width={200}
+                  height={200}
+                  alt="pass image"
+                  src={formValues.passImage}
+                />}
+              </div>
             </div>
-            </div></div>
+          </div>
         </div>
       </div>
       {/*  */}
 
       <div className="max-w-xl mx-auto">
         <Button
-          copy="Preview collection"
+          copy="Chat with Insider to get started"
           variant="primary"
           onClick={() => {
-            setPreview(true);
+           window.location = 'https://calendly.com/foundersinsider'
           }}
         />
       </div>
@@ -264,9 +274,14 @@ const GetStarted = () => {
 export default GetStarted;
 
 const Section = (props) => (
-  <div className="h-screen flex" ref={props.ref}> 
-  <section className="m-auto border border-black p-4 pb-12 w-full max-w-xl mx-auto font-para grid">
-    <>{props.children}</>
-  </section>
+  <div className="h-screen flex">
+    <section className="m-auto border border-black p-4 pb-12 w-full max-w-xl mx-auto font-para grid">
+      <>{props.children}</>
+    </section>
   </div>
 );
+
+const Tag = () => {
+  const styles = clsx("px-4 py-1");
+  return <p className={styles}> tag </p>;
+};
--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -2,3 +2,6 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 // https://firebase.google.com/docs/auth/web/google-signin?authuser=0#before_you_begin
+const SignIn = () => <p> Sign in </p>;
+
+export default SignIn;
